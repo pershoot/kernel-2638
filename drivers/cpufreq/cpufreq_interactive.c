9cdc300db3ddc034e1c963594d274d55371c5e26
Allen Martin <amartin@nvidia.com>
2011-07-19 14:56:21 -0700

    cpufreq: interactive: fix checkpatch warnings on long lines
    Fix up checkpatch warning introduced by long lines in timer_rate
    patch.
    
    Change-Id: I22b105dafb1b49390799bb7577464da03f0f8afb
    Signed-off-by: Allen Martin <amartin@nvidia.com>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -266,7 +266,8 @@ rearm:
 
 		pcpu->time_in_idle = get_cpu_idle_time_us(
 			data, &pcpu->idle_exit_time);
-		mod_timer(&pcpu->cpu_timer, jiffies + usecs_to_jiffies(timer_rate));
+		mod_timer(&pcpu->cpu_timer,
+			  jiffies + usecs_to_jiffies(timer_rate));
 	}
 
 exit:
@@ -302,7 +303,8 @@ static void cpufreq_interactive_idle(void)
 			pcpu->time_in_idle = get_cpu_idle_time_us(
 				smp_processor_id(), &pcpu->idle_exit_time);
 			pcpu->timer_idlecancel = 0;
-			mod_timer(&pcpu->cpu_timer, jiffies + usecs_to_jiffies(timer_rate));
+			mod_timer(&pcpu->cpu_timer,
+				  jiffies + usecs_to_jiffies(timer_rate));
 		}
 #endif
 	} else {
@@ -346,7 +348,8 @@ static void cpufreq_interactive_idle(void)
 			get_cpu_idle_time_us(smp_processor_id(),
 					     &pcpu->idle_exit_time);
 		pcpu->timer_idlecancel = 0;
-		mod_timer(&pcpu->cpu_timer, jiffies + usecs_to_jiffies(timer_rate));
+		mod_timer(&pcpu->cpu_timer,
+			  jiffies + usecs_to_jiffies(timer_rate));
 	}
 
 }
